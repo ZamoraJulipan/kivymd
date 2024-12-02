e1f9e88db68bb1a9c566cb57fd6fe69a2e29320c
ZamoraJulipan <134815443+ZamoraJulipan@users.noreply.github.com>
2024-12-02 15:11:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.core.window import Window
+
+KV = '''
+FloatLayout:
+    MDLabel:
+        id: t
+        text: "El tamaño es: "
+        font_style: "H2"
+    MDFlatButton:
+        text: 'Revisar'
+        font_style: "H4"
+        on_press: app.medir()
+'''
 
 class main(MDApp):
     def build(self):
-        return Builder.load_file('main.kv')
+        return Builder.load_string(KV)
+    
+    def medir(self):
+        self.root.ids.t.text = str(Window.size)
 
 main().run()
